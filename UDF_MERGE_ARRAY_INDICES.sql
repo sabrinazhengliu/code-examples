f133ad70d0e4aff3e133e825d3e8887321e29e44
Sabrina Liu <70238496+sabrinazhengliu@users.noreply.github.com>
2025-09-05 17:26:14 -0600

    Update UDF_MERGE_ARRAY_INDICES.sql

--- UDF_MERGE_ARRAY_INDICES.sql
+++ UDF_MERGE_ARRAY_INDICES.sql
@@ -9,15 +9,12 @@ def merge_array_indices(event_schema: list) -> list:
     def replace_idx(schema_pair: dict):
         schema_pair['COLUMN_NAME'] = re.sub(r"\[\d+\]", "[ARRAY]", schema_pair['COLUMN_NAME'])
         return schema_pair
-
     return_list = [
         replace_idx(sch)
         if "[" in sch['COLUMN_NAME']
         else sch
         for sch in event_schema
     ]
-    
-
     return [dict(t) for t in {tuple(d.items()) for d in return_list}]
 $$
 ;
@@ -26,3 +23,11 @@ select UDF_MERGE_ARRAY_INDICES([
   PARSE_JSON('{"COLUMN_NAME": "c1[0].key", "TYPE": "TEXT"}')
 , PARSE_JSON('{"COLUMN_NAME": "c1[1].key", "TYPE": "TEXT"}')
 ])
+;
+
+# result: [
+  {
+    "COLUMN_NAME": "c1[ARRAY].key",
+    "TYPE": "TEXT"
+  }
+]
