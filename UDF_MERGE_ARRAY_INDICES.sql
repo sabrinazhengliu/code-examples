1557316f5f389b7d7d29dc4c607f989ad432ba94
Sabrina Liu <70238496+sabrinazhengliu@users.noreply.github.com>
2025-09-10 17:35:23 -0600

    Update UDF_MERGE_ARRAY_INDICES.sql

--- UDF_MERGE_ARRAY_INDICES.sql
+++ UDF_MERGE_ARRAY_INDICES.sql
@@ -31,3 +31,62 @@ select UDF_MERGE_ARRAY_INDICES([
     "TYPE": "TEXT"
   }
 ]
+
+
+import re
+def merge_json_keys(json_keys: list) -> list:
+    return_list = set([
+        re.sub(r"\[\d+\]", "[0]", key)
+        if "[" in key
+        else key
+        for key in json_keys
+    ])
+    root_list = []
+    for key in return_list:
+        if "[0]" in key:
+            root = key.split("[0]", 1)[0]
+            root_list.append(root)
+    for key in set(root_list):
+        return_list.discard(key)
+    return sorted(list(return_list))
+
+keys = [
+    "Level1",
+    "Level1[0].A",
+    "Level1[0].B",
+    "Level1[0].C",
+    "Level1[1].A",
+    "Level1[1].B",
+    "Level1[1].C",
+    "Level1[2].A",
+    "Level1[2].B",
+    "Level1[2].C",
+    "Other",
+]
+
+merge_json_keys(keys)
+
+def convert_float_to_decimal(json_data: dict) -> str:
+    keep_keys = merge_json_keys(json_data.keys())
+
+    def mapper(key: str) -> str:
+        data = f"EVENT_PAYLOAD:{key}"
+        stmt = f"'{key}', CASE WHEN IS_REAL({data}) THEN TO_DECIMAL({data}, 38, 9) ELSE {data} END"
+        return stmt
+
+    query = "OBJECT_CONSTRUCT(\n"
+    query += "\n,".join([mapper(key) for key in keep_keys])
+    query += "\n) AS SAMPLE_PAYLOAD"
+    return query
+
+d ={
+    "L1": 1,
+    "L1[0].A": 1,
+    "L1[0].B": 1,
+    "L1[1].A": 1,
+    "L1[1].B": 1,
+    "L1[2].A": 1,
+    "L1[2].B": 1,
+    "OTHER": 0}
+
+convert_float_to_decimal(d)
